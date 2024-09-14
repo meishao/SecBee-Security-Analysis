a6f40d023a388b179b181397b81d018001ffc8f2
$H0 <meishao2002@msn.com>
2024-09-15 03:48:20 +0900

    Update super-admin.py

--- app_pages/super-admin.py
+++ app_pages/super-admin.py
@@ -1,11 +1,7 @@
 import streamlit as st
-from menu import menu_with_redirect
-
-# Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
 
 # Verify the user's role
-if st.session_state.role not in ["super-admin"]:
+if not st.session_state.logged_in:
     st.warning("You do not have permission to view this page.")
     st.stop()
 
