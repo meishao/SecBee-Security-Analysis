a928e19c0e738b1791c2d1fb20052159345c8d6a
$H0 <meishao2002@msn.com>
2024-09-14 13:20:33 +0900

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("app.py", label="Switch accounts")
+    st.sidebar.page_link("streamlit_app.py", label="Switch accounts")
     st.sidebar.page_link("pages/user.py", label="Your profile")
     if st.session_state.role in ["admin", "super-admin"]:
         st.sidebar.page_link("pages/admin.py", label="Manage users")
@@ -16,7 +16,7 @@ def authenticated_menu():
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
-    st.sidebar.page_link("app.py", label="Log in")
+    st.sidebar.page_link("streamlit_app.py", label="Log in")
 
 
 def menu():
@@ -32,5 +32,5 @@ def menu_with_redirect():
     # Redirect users to the main page if not logged in, otherwise continue to
     # render the navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
-        st.switch_page("app.py")
+        st.switch_page("streamlit_app.py")
     menu()
