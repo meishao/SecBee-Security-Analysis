3936f6a56d2654b07cbcb0e0057c031604befd67
$H0 <meishao2002@msn.com>
2024-09-14 13:55:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,13 @@ from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(layout="wide")
 
-sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+# sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
-nav = get_nav_from_toml(
-    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
-)
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+# nav = get_nav_from_toml(
+#    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+#)
 
 st.logo("secbee-high-resolution-logo-transparent.png")
 
