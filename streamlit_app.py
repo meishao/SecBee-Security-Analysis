fe814f05dac58e4ac582d94fc3827ac563b9154d
$H0 <meishao2002@msn.com>
2024-09-15 03:45:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def dashboard():
     )
     search_page = st.Page("app_pages/snort_rule.py", title="搜索", icon=":material/search:")
     history_page = st.Page("app_pages/admin.py", title="历史记录", icon=":material/history:")
-    logout_page = st.Page(logout(supabase), title="退出登录", icon=":material/logout:")
+    logout_page = st.Page(logout, title="退出登录", icon=":material/logout:")
 
     # 导航
     pg = st.navigation(
@@ -38,6 +38,13 @@ def dashboard():
     pg.run()
 
 def logout():
+    st.title("退出登录")
+    st.write("正在退出...")
+    if st.button("退出"):
+        # 使用 Supabase 进行登出
+        supabase.auth.sign_out()
+        st.session_state.clear()
+        st.rerun()
 
 def main():
     if not check_auth():
