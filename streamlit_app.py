46aa17619fef783f879712f7df0c309d1d717444
$H0 <meishao2002@msn.com>
2024-09-14 12:37:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("SecBee AI Security Analysis")
 uploaded_file = st.file_uploader("Upload analysis file:")
-if uploader_file is not None:
+if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
 
 if st.button("Start"):
