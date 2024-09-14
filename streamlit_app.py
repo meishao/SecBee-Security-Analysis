ff46e1a3a001c8684ca45ba4238a495f66079a4d
$H0 <meishao2002@msn.com>
2024-09-14 12:19:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+"""
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+"""
+
+st.title("SecBee AI Security Analysis")
+fp = st.text_input("Upload analysis file:")
+
+if st.button("Start"):
+    st.write("Analyzing...")
